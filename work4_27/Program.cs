6cc956b3ceeaf5d9b83d03016971c4dd237ef05b
Dmitriy <timon@yahoo.com>
2022-12-08 13:04:01 +0300

    Задание 27

---
+++ work4_27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27. Найти сумму цифр в числе
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+    {
+        s = s + a % 10;
+        a = a /10;
+    }
+Console.WriteLine("Сумма цифр в числе равна " + s);
