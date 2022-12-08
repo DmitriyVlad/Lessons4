193102559319617a61e16e7d10de84f6bdc92a89
Dmitriy <timon@yahoo.com>
2022-12-08 11:06:47 +0300

    Задание 25

---
+++ work4_25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25
+
+int a = new Random().Next(1, 5);
+int b = new Random().Next(1, 5);
+int s = a;
+for(int i = 0; i < b - 1; i++)
+{
+    s = s * a;
+}
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine(a + " в степени " + b + " равно: "+ s);
