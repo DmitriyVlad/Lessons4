68949297e1c7c70fc8825c62ca9fff335402b397
Dmitriy <timon@yahoo.com>
2022-12-08 17:16:41 +0300

    Задание 29

---
+++ work4_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29 Заполнение восьмимерного массива числами
+
+int[] arr = new int[8];
+int i = 0;
+while (i < arr.Length)
+{
+    arr[i] = new Random().Next(1, 100);
+    i++;
+}
+for (int k = 0; k < arr.Length; k++)
+{
+    Console.Write(arr[k] + " ");
+}
+Console.WriteLine();
